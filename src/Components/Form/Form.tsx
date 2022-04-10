d9d1e07903df4c2d919abbc5ec27fb81d228acd1
sviridofficial <kostya.sviridov.01@mail.ru>
2022-04-10 16:55:21 +0700

    feat: add responsive design

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -7,13 +7,15 @@ import Button from "../Button/Button";
 
 const Form: React.FC = () => {
     return (
-        <div className={styles.Form}>
-            <FormHeader headerText={"Войти"}/>
-            <FormInput inputLabel={"Логин"} placeholder={"Введите логин"}/>
-            <FormInput inputLabel={"Пароль"} placeholder={"Введите пароль"}/>
-            <Button label={"Войти"}/>
-            <div className={styles.forgotPasswordBlock}>
-                <a className={styles.forgotPassword} href={'/'}>Забыли пароль?</a>
+        <div className={styles.FormContainer}>
+            <div className={styles.Form}>
+                <FormHeader headerText={"Войти"}/>
+                <FormInput inputLabel={"Логин"} placeholder={"Введите логин"}/>
+                <FormInput inputLabel={"Пароль"} placeholder={"Введите пароль"}/>
+                <Button label={"Войти"}/>
+                <div className={styles.forgotPasswordBlock}>
+                    <a className={styles.forgotPassword} href={'/'}>Забыли пароль?</a>
+                </div>
             </div>
         </div>
     )
--- src/Components/Form/FormInput/FormInput.tsx
+++ src/Components/Form/FormInput/FormInput.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./FormInput.module.css";
+import {ReactComponent as Show} from "../../../Assets/Show.svg";
+import {ReactComponent as Hide} from "../../../Assets/Hide.svg";
+import {ReactComponent as Info} from "../../../Assets/Info.svg";
 
 export interface FormInputProps {
     inputLabel: string
@@ -11,6 +14,11 @@ const FormInput: React.FC<FormInputProps> = ({inputLabel, placeholder}) => {
         <div className={styles.formInput}>
             <p className={styles.inputHeader}>{inputLabel}</p>
             <input className={styles.input} placeholder={placeholder}></input>
+            <div className={styles.notification}>
+                <div className={styles.show}><Show/></div>
+                <div className={styles.hide}><Hide/></div>
+                <div className={styles.info}><Info/></div>
+            </div>
         </div>
     )
 }
