172ab59ddc98753ec1eb93bfa9533baad131a6a3
sviridofficial <kostya.sviridov.01@mail.ru>
2022-05-12 01:35:36 +0700

    feat: finished 4.5

--- src/Components/AboutMeContent/AboutMeContent.tsx
+++ src/Components/AboutMeContent/AboutMeContent.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from "./AboutMeContent.module.css";
 import {ReactComponent as Photo} from "../../Assets/Camera.svg";
 import {ReactComponent as Pencil} from "../../Assets/pencil.svg";
@@ -6,11 +6,14 @@ import FormInput from "../FormInput/FormInput";
 import DropdownForm from "../DropdownForm/DropdownForm";
 import TextareaForm from "../TextareaForm/TextareaForm";
 import {useStore} from "effector-react";
-import {$userStore, changeUserInformation, validateFields} from "../../State/userStore";
+import {$userStore, changeUserInformation, getYearsOld, validateFields} from "../../State/userStore";
 import ErrorEditBlock from "../ErrorEditBlock/ErrorEditBlock";
 import {notificationOpen} from "../../State/notifacationStore";
 import PhotoUpload from "../PhotoUpload/PhotoUpload";
 import photo from "../../Pages/MainPage/Components/MainSection/AboutMe/KonstantinPhoto.jpg";
+import {getUserInfo} from "../../State/api";
+import {useNavigate} from "react-router-dom";
+import Loader from "../Loader/Loader";
 
 interface IAboutMeContent {
     setError(value: object): void
@@ -18,7 +21,9 @@ interface IAboutMeContent {
 
 const AboutMeContent: React.FC<IAboutMeContent> = ({setError}) => {
     const inputRef = useRef(null);
+    const auth = useNavigate();
     const userData = useStore($userStore);
+    const [isLoading, setIsLoading] = useState(true);
     const [isEdit, setIsEdit] = useState(false);
     const [firsname, setFirstname] = useState(userData.username);
     const [lastname, setLastname] = useState(userData.lastname);
@@ -28,6 +33,8 @@ const AboutMeContent: React.FC<IAboutMeContent> = ({setError}) => {
     const [pet, setPet] = useState(userData.pet);
     const [shortInformation, setShortInformation] = useState(userData.shortInformation);
     const [aboutMe, setAboutMe] = useState(userData.aboutMe);
+    const [image, setImage] = useState("");
+
     const saveData = () => {
         const newData = {
             username: firsname,
@@ -41,6 +48,7 @@ const AboutMeContent: React.FC<IAboutMeContent> = ({setError}) => {
             imagePath: ""
         }
         const checkErrors = validateFields(newData);
+
         if (checkErrors !== true) {
             setError({isActive: true, errorMessage: checkErrors})
         } else {
@@ -59,46 +67,72 @@ const AboutMeContent: React.FC<IAboutMeContent> = ({setError}) => {
         // @ts-ignore
         inputRef.current.click();
     }
+    useEffect(() => {
+        const request = async () => {
+            const result = await getUserInfo();
+            if (result.statusCode === 401) {
+                auth("/");
+            } else {
+                const birthday = new Date(result.birthDate);
+                setFirstname(result.firstName);
+                setLastname(result.lastName);
+                setDate(birthday.getDate() + "." + (birthday.getMonth() + 1) + "." + birthday.getFullYear())
+                setAboutMe(result.aboutMe);
+                setPet(result.hasPet ? "Есть" : "Нет");
+                setSex(result.gender === "male" ? "Мужчина" : "Женщина");
+                setCity(result.cityOfResidence == "Tomsk" ? "Томск" : result.cityOfResidence);
+                setShortInformation(result.smallAboutMe == null ? "" : result.smallAboutMe);
+                setImage("https://academtest.ilink.dev/images/" + result.profileImage)
+            }
+            setIsLoading(false);
+        }
+        request();
+    }, [])
     return (
         <div>
             <h1 className={styles.aboutMeHeaderText}>Обо мне</h1>
-            <div className={styles.blockPhoto}>
-                <div className={styles.photoContainer}>
-                    <img src={photo} className={styles.photo}/>
-                    <div className={styles.hoverPhoto}><img src={photo} className={styles.photo}/></div>
-                    <div className={styles.changePhoto}>
-                        <h1 className={styles.userPhotoText}>Фото профиля</h1>
-                        <div className={styles.changePhotoText} onClick={changePhotoClick}>
-                            <Pencil className={styles.pencil}/>
-                            <input type='file' id="real-file" hidden={true} ref={inputRef} onChange={(e) => {
-                                // @ts-ignore
-                                console.log(e.target.files[0].name);
-                            }}/>
-                            <p>Изменить фото</p>
+            {isLoading ? <Loader/> :
+                <div>
+                    <div className={styles.blockPhoto}>
+                        <div className={styles.photoContainer}>
+                            <img src={image} className={styles.photo}/>
+                            <div className={styles.hoverPhoto}><img src={image} className={styles.photo}/></div>
+                            <div className={styles.changePhoto}>
+                                <h1 className={styles.userPhotoText}>Фото профиля</h1>
+                                <div className={styles.changePhotoText} onClick={changePhotoClick}>
+                                    <Pencil className={styles.pencil}/>
+                                    <input type='file' id="real-file" hidden={true} ref={inputRef} onChange={(e) => {
+                                        // @ts-ignore
+                                        console.log(e.target.files[0].name);
+                                    }}/>
+                                    <p>Изменить фото</p>
+                                </div>
+
+                            </div>
                         </div>
-
+                        <button onClick={() => {
+                            setIsEdit(true)
+                        }} className={isEdit === true ? styles.none : styles.editButton}>Редактировать
+                        </button>
                     </div>
-                </div>
-                <button onClick={() => {
-                    setIsEdit(true)
-                }} className={isEdit === true ? styles.none : styles.editButton}>Редактировать
-                </button>
-            </div>
-            <div className={styles.changeFormBlock}>
-                <FormInput fieldName={"Имя"} value={firsname} setValue={setFirstname} isEdit={isEdit}/>
-                <FormInput fieldName={"Фамилия"} value={lastname} setValue={setLastname} isEdit={isEdit}/>
-                <FormInput fieldName={"Дата рождения"} value={date} setValue={setDate} isEdit={isEdit}/>
-                <DropdownForm fieldName={"Город"} type={"city"} value={city} setValue={setCity} isEdit={isEdit}/>
-                <DropdownForm fieldName={"Пол"} type={"sex"} value={sex} setValue={setSex} isEdit={isEdit}/>
-                <DropdownForm fieldName={"Животное"} type={"pet"} value={pet} setValue={setPet} isEdit={isEdit}/>
-            </div>
-            <TextareaForm fieldName={"Краткая информация"} type={"Краткая информация"} value={shortInformation}
-                          setValue={setShortInformation} isEdit={isEdit}/>
-            <TextareaForm fieldName={"О себе"} type={"О себе"} value={aboutMe} setValue={setAboutMe} isEdit={isEdit}/>
-            <div className={isEdit === false ? styles.none : styles.saveButtonContainer}>
-                <button className={styles.saveButton} onClick={saveData}>Сохранить изменения</button>
-            </div>
-
+                    <div className={styles.changeFormBlock}>
+                        <FormInput fieldName={"Имя"} value={firsname} setValue={setFirstname} isEdit={isEdit}/>
+                        <FormInput fieldName={"Фамилия"} value={lastname} setValue={setLastname} isEdit={isEdit}/>
+                        <FormInput fieldName={"Дата рождения"} value={date} setValue={setDate} isEdit={isEdit}/>
+                        <DropdownForm fieldName={"Город"} type={"city"} value={city} setValue={setCity}
+                                      isEdit={isEdit}/>
+                        <DropdownForm fieldName={"Пол"} type={"sex"} value={sex} setValue={setSex} isEdit={isEdit}/>
+                        <DropdownForm fieldName={"Животное"} type={"pet"} value={pet} setValue={setPet}
+                                      isEdit={isEdit}/>
+                    </div>
+                    <TextareaForm fieldName={"Краткая информация"} type={"Краткая информация"} value={shortInformation}
+                                  setValue={setShortInformation} isEdit={isEdit}/>
+                    <TextareaForm fieldName={"О себе"} type={"О себе"} value={aboutMe} setValue={setAboutMe}
+                                  isEdit={isEdit}/>
+                    <div className={isEdit === false ? styles.none : styles.saveButtonContainer}>
+                        <button className={styles.saveButton} onClick={saveData}>Сохранить изменения</button>
+                    </div>
+                </div>}
         </div>
     )
 }
--- src/Components/ReviewBlock/ReviewBlock.tsx
+++ src/Components/ReviewBlock/ReviewBlock.tsx
@@ -6,6 +6,10 @@ import {ReactComponent as RejectedLogo} from "../../Assets/Close Square.svg";
 import {ReactComponent as PublishedLogo} from "../../Assets/published.svg";
 import ModalEditReview from "../ModalEditReview/ModalEditReview";
 import {ReactComponent as EmptyPhoto} from "../../Assets/Camera.svg";
+import {changeReviewState} from "../../State/api";
+import {useNavigate} from "react-router-dom";
+import {notificationOpen} from "../../State/notifacationStore";
+import Loader from "../Loader/Loader";
 
 interface IReviewProps {
     id: string,
@@ -18,11 +22,31 @@ interface IReviewProps {
 
 const ReviewBlock: React.FC<IReviewProps> = ({id, username, createdAt, comment, type, authorImage}) => {
     const [modalActive, setModalActive] = useState(false);
-    const rejectClick = () => {
-        rejectReview(id);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigate();
+    const rejectClick = async () => {
+        setIsLoading(true);
+        const response = await changeReviewState(id, "declined").then(response => {
+            if (response.ok) {
+                rejectReview(id);
+                notificationOpen({isOpen: true, type: "success", headerValue: "Успешно!", value: "Отзыв отклонен..."})
+                setIsLoading(false);
+            } else {
+                navigation("/");
+            }
+        })
     }
-    const publishClick = () => {
-        publishReview(id);
+    const publishClick = async () => {
+        setIsLoading(true);
+        const response = await changeReviewState(id, "approved").then(response => {
+            if (response.ok) {
+                publishReview(id);
+                notificationOpen({isOpen: true, type: "success", headerValue: "Успешно!", value: "Отзыв опубликован..."})
+                setIsLoading(false);
+            } else {
+                navigation("/");
+            }
+        })
     }
     const editClick = () => {
         setModalActive(true);
@@ -30,38 +54,40 @@ const ReviewBlock: React.FC<IReviewProps> = ({id, username, createdAt, comment, 
     return (
         <div
             className={type === "declined" ? styles.reviewBlockRejected : type === "approved" ? styles.reviewBlockPublished : styles.reviewBlock}>
-            <div>
-                <div className={styles.reviewHeader}>
-                    <div className={styles.reviewHeaderUserInformation}>
-                        <div className={styles.photo}>
-                            {authorImage != null ? <img className={styles.userPhoto}
-                                                        src={"https://academtest.ilink.dev/images/" + authorImage}/>
-                                : <div className={styles.photoUserNull}><EmptyPhoto/></div>}
+            {isLoading ? <Loader/> : <div>
+                <div>
+                    <div className={styles.reviewHeader}>
+                        <div className={styles.reviewHeaderUserInformation}>
+                            <div className={styles.photo}>
+                                {authorImage != null ? <img className={styles.userPhoto}
+                                                            src={"https://academtest.ilink.dev/images/" + authorImage}/>
+                                    : <div className={styles.photoUserNull}><EmptyPhoto/></div>}
 
+                            </div>
+                            <p className={styles.userName}>{username}</p>
                         </div>
-                        <p className={styles.userName}>{username}</p>
+                        <p className={styles.date}>{new Date(createdAt).getDate() + "." + (new Date(createdAt).getMonth() + 1) + "." + new Date(createdAt).getFullYear()}</p>
                     </div>
-                    <p className={styles.date}>{new Date(createdAt).getDate() + "." + (new Date(createdAt).getMonth() + 1) + "." + new Date(createdAt).getFullYear()}</p>
+                    <p className={styles.comment}>{comment}</p>
                 </div>
-                <p className={styles.comment}>{comment}</p>
-            </div>
-            {type === "declined" ?
-                <div className={styles.rejected}>
-                    <RejectedLogo/>
-                    <p className={styles.rejectedText}>Отзыв отклонен</p>
-                </div> : type === "approved" ?
-                    <div className={styles.published}>
-                        <PublishedLogo/>
-                        <p className={styles.publishedText}>Отзыв опубликован</p>
-                    </div>
-                    :
-                    <div className={styles.buttons}>
-                        <div className={styles.leftButtons}>
-                            <button onClick={publishClick} className={styles.submit}>Опубликовать</button>
-                            <button onClick={rejectClick} className={styles.refuse}>Отклонить</button>
+                {type === "declined" ?
+                    <div className={styles.rejected}>
+                        <RejectedLogo/>
+                        <p className={styles.rejectedText}>Отзыв отклонен</p>
+                    </div> : type === "approved" ?
+                        <div className={styles.published}>
+                            <PublishedLogo/>
+                            <p className={styles.publishedText}>Отзыв опубликован</p>
                         </div>
-                        <button onClick={editClick} className={styles.edit}><Edit/></button>
-                    </div>}
+                        :
+                        <div className={styles.buttons}>
+                            <div className={styles.leftButtons}>
+                                <button onClick={publishClick} className={styles.submit}>Опубликовать</button>
+                                <button onClick={rejectClick} className={styles.refuse}>Отклонить</button>
+                            </div>
+                            <button onClick={editClick} className={styles.edit}><Edit/></button>
+                        </div>}
+            </div>}
             <ModalEditReview comment={comment} id={id} active={modalActive} setActive={setModalActive}/>
         </div>
     )
